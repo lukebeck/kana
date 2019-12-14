a4f28e40b90dc10803fff5408bd8644287fc3cb4
Luke <lukembeck@gmail.com>
2019-12-14 11:32:42 +1000

    Added basic data and import function
    Created a data.js file with some kana and modified App.js to load this data into the app.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import shuffle from 'array-shuffle'
 import theme from './theme'
+import data from './data'
 
 function App() {
+  const input = shuffle(
+    data.map(item => ({ ...item, answered: 0, correct: 0 }))
+  )
+
   return (
     <React.Fragment>
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <Container maxWidth='xs'>
-          <div>かな</div>
+          <div>{input[0].hiragana}</div>
         </Container>
       </ThemeProvider>
     </React.Fragment>
---
+++ src/data.js
@@ -0,0 +1,64 @@
+const kana = [
+  {
+    hiragana: 'あ',
+    katakana: 'ア',
+    romaji: 'a',
+    group: 1
+  },
+  {
+    hiragana: 'い',
+    katakana: 'イ',
+    romaji: 'i',
+    group: 1
+  },
+  {
+    hiragana: 'う',
+    katakana: 'ウ',
+    romaji: 'u',
+    group: 1
+  },
+  {
+    hiragana: 'え',
+    katakana: 'エ',
+    romaji: 'e',
+    group: 1
+  },
+  {
+    hiragana: 'お',
+    katakana: 'オ',
+    romaji: 'o',
+    group: 1
+  },
+  {
+    hiragana: 'か',
+    katakana: 'カ',
+    romaji: 'ka',
+    group: 2
+  },
+  {
+    hiragana: 'き',
+    katakana: 'キ',
+    romaji: 'ki',
+    group: 2
+  },
+  {
+    hiragana: 'く',
+    katakana: 'ク',
+    romaji: 'ku',
+    group: 2
+  },
+  {
+    hiragana: 'け',
+    katakana: 'ケ',
+    romaji: 'ke',
+    group: 2
+  },
+  {
+    hiragana: 'こ',
+    katakana: 'コ',
+    romaji: 'ko',
+    group: 2
+  }
+]
+
+export default kana
